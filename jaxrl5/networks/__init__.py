9ef5732cbdc4b91d24cb470f16bc884b37cdaff6
jgsimard <jeangab23@gmail.com>
2023-11-05 09:58:41 -0500

    add missing files

--- jaxrl5/networks/__init__.py
+++ jaxrl5/networks/__init__.py
@@ -1,15 +1,34 @@
+from jaxrl5.networks.diffusion import (
+    DDPM,
+    FourierFeatures,
+    cosine_beta_schedule,
+    ddpm_sampler,
+    vp_beta_schedule,
+)
 from jaxrl5.networks.ensemble import Ensemble, subsample_ensemble
-from jaxrl5.networks.mlp import MLP, default_init
+from jaxrl5.networks.mlp import MLP, default_init, get_weight_decay_mask
 from jaxrl5.networks.mlp_resnet import MLPResNetV2
+from jaxrl5.networks.pixel_multiplexer import PixelMultiplexer
+from jaxrl5.networks.resnet import MLPResNet
 from jaxrl5.networks.state_action_nextstate import StateActionNextState
 from jaxrl5.networks.state_action_value import StateActionValue
+from jaxrl5.networks.state_value import StateValue
 
 __all__ = [
     "Ensemble",
     "subsample_ensemble",
     "MLP",
     "default_init",
+    "get_weight_decay_mask",
     "MLPResNetV2",
     "StateActionNextState",
     "StateActionValue",
+    "DDPM",
+    "FourierFeatures",
+    "cosine_beta_schedule",
+    "ddpm_sampler",
+    "vp_beta_schedule",
+    "MLPResNet",
+    "PixelMultiplexer",
+    "StateValue",
 ]
--- jaxrl5/networks/mlp.py
+++ jaxrl5/networks/mlp.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable, Sequence
 
+import flax
 import flax.linen as nn
 import jax.numpy as jnp
 from jax import Array
@@ -7,6 +8,17 @@ from jax import Array
 
 def default_init(scale: float | None = jnp.sqrt(2)):
     return nn.initializers.orthogonal(scale)
+
+
+def get_weight_decay_mask(params):
+    flattened_params = flax.traverse_util.flatten_dict(flax.core.frozen_dict.unfreeze(params))
+
+    def decay(k):
+        return not any((key == "bias" or "Input" in key or "Output" in key) for key in k)
+
+    return flax.core.frozen_dict.freeze(
+        flax.traverse_util.unflatten_dict({k: decay(k) for k in flattened_params})
+    )
 
 
 class MLP(nn.Module):
